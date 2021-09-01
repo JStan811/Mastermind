19fb11868b3f035243d95c88fb21a9775d7295d5
Jake Standley <jakestandley95@gmail.com>
2021-09-01 14:03:25 -0500

    added code board class file

---
+++ lib/code_board.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Mastermind
+  # board guesser/codebreaker uses
+  class CodeBoard
+    def initialize
+      @state = Array.new(4) # array of 4 positions, all with a value of a color (or number?) Or should the value be a peg?
+    end
+
+    # possible methods: read/write state (read board state / place pegs), reset, display board
+    attr_accessor :state
+
+    def reset
+      @state = Array.new(4)
+    end
+
+    def display_board
+      # print user friendly display of board
+    end
+  end
+end
